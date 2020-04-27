b3feee6366723cebb34c371c745ebe859af397b1
suhao <638781@qq.com>
2020-04-27 17:29:27 +0800

    Create RunFirstAsAdmin.bat

---
+++ tools/RunFirstAsAdmin.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
+Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo 请使用右键管理员身份运行！&&PAUSE >NUL&&EXIT)
+Rd "%WinDir%\System32\test_permissions" 2>NUL
